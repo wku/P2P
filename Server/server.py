bbb086aa1cc3b06c83637d6936669029d9dfd4ba
ccgogo123 <xhhuangxi@gmail.com>
2013-10-16 23:15:20 -0500

    simple server

--- Server/server.py
+++ Server/server.py
@@ -3,6 +3,21 @@ Created on Oct 16, 2013
 
 @author: christopherhuang
 '''
+from socket import *
 
 if __name__ == '__main__':
-    pass
+    myHost = ''
+    myPort = '50007'
+    
+    sockObj = socket(AF_INET, SOCK_STREAM)
+    sockObj.bind(myHost, myPort)
+    sockObj.listen(5)
+    
+    while True:
+        connection, address = sockObj.accept()
+        print('Serer connected by', address)
+        while True:
+            data = connection.recv(1024)
+            if not data: break
+            connection.send(b'Echo=>' + data)
+            connection.close()
