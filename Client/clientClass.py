848773a662dc96b3dd224fac9e54fc4529b901f8
ccgogo123 <xhhuangxi@gmail.com>
2013-10-22 20:14:31 -0500

    Milestone stage 1
    Finish forming the P2P system. Next I will implement 4 required
    commands.

--- Client/clientClass.py
+++ Client/clientClass.py
@@ -16,14 +16,15 @@ class Client:
         self.myAddress = (host, port)
         self.serverHost = 'localhost'
         self.serverPort = 50007
-        self.neighbour = []
+        self.neighbour = []                     #neighbour list
         
-    def isConfirmed(self, address, sockObj):  
-
+    def isConfirmed(self, address):  
+        sockObj = socket(AF_INET, SOCK_STREAM)
         message = 'join'
         
         try:
-            sockObj.connect(address)   #connect to the neighbor
+            print address
+            sockObj.connect((address[0], int(address[1])))   #connect to the neighbor
         except error:
             print "fail to connect peer"   
         
@@ -43,14 +44,17 @@ class Client:
         '''
         join the P2P system
         '''
-        request = 'join'                                    #join request
+        request = 'join' + ' ' + str(self.myAddress[1])               #join request
         sockObj = socket(AF_INET, SOCK_STREAM)
+        
         try:
             sockObj.connect((self.serverHost, self.serverPort)) #connect to the server
         except error:
             print 'connection error'                        #if the connection fails
+            return False                                    #return false
         
         sockObj.send(request)                               #send join request
+        print "send request to server", self.serverHost, self.serverPort
         
         while True:
             response = sockObj.recv(1024)                       #response from server
@@ -59,10 +63,10 @@ class Client:
                 sockObj.close()
                 return True                                 #join successfully
             else:
-                address = tuple(response.split(' '))               #retrieve the host name and port number
+                address = response.split(' ')               #retrieve the host name and port number
                 sockObj.close()                             #close the connection to the server 
                        
-                if self.isConfirmed(address, sockObj):      #connect the peer
+                if self.isConfirmed(address):      #connect the peer
                     self.neighbour.append(address)
                     return True                             #join successfully
                 else:
@@ -70,6 +74,7 @@ class Client:
     
                    
     def main(self):
+
         '''
         main function
         '''
@@ -79,7 +84,8 @@ class Client:
             sockObj.bind(self.myAddress)                        #bind it to client's address
             sockObj.listen(5)                               #start to listen the coming message
             print "join the P2P system successfully"
-            
+            print "waiting"
+            print self.neighbour
         while True:
             connection, address = sockObj.accept()
             print "Client connected " , address
--- Server/server.py
+++ Server/server.py
@@ -10,16 +10,17 @@ def register(address, peerList):
     '''
     register a new peer into the system
     '''
-    if len(peerList) == 0:
+    if len(peerList) == 0:                  #there is no peer in the system
         peerList.append(address)
         return "success"
     else:
+        print "at regester\n"               #store newly coming peer
         if address not in peerList:
-            peerList.append(list(address))
-            peer = peerList[random.randint(0,len(peerList) - 2)]
+            peerList.append(address)
+            peer = peerList[random.randint(0,len(peerList) - 2)]        #randomly choose peer from the list
             peer = str(peer[0]) + ' ' + str(peer[1])
             return peer
-        else:
+        else:                                                           #duplicated address
             return 'already registered'
     
 if __name__ == '__main__':
@@ -34,15 +35,18 @@ if __name__ == '__main__':
     while True:
         connection, address = sockObj.accept()
         print('Server connected by', address)
-        print type(address)
         while True:
             data = connection.recv(1024)
+            data = data.split(' ')
+            print data
             if not data: 
                 break
-            if data == 'join':
+            if data[0] == 'join':                       #one peer wants to join the system
+                address = list(address)                     
+                address[1] = data[1]                     #switch listening port over the connection port number
                 outcome = register(address, peerList)                
-                connection.send(outcome)
-                print peerList
-                
+                connection.send(outcome)                #respond the peer
+                print peerList                          #print the peer already in the system
+                break                                   #finish the response this time
         connection.close()
             
